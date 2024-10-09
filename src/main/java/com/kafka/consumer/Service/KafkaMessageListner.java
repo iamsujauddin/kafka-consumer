3b8b0da61fad2e6463341239d2ad7cc74dad1fd0
Sk Sujauddin <suja.hash69@gmail.com>
2024-10-09 11:55:07 +0530

    second commit

--- src/main/java/com/kafka/consumer/Service/KafkaMessageListner.java
+++ src/main/java/com/kafka/consumer/Service/KafkaMessageListner.java
@@ -1,5 +1,6 @@
-package com.kafka.consumer.Service;
+		package com.kafka.consumer.Service;
 
+import com.kafka.consumer.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,8 +11,13 @@ public class KafkaMessageListner {
 
 	Logger log=LoggerFactory.getLogger(KafkaMessageListner.class);
 	
-	@KafkaListener(topics = "kafka_demo_new",groupId = "kafka-grp1")
-	public void consueme(String message) {
-		log.info("message cosumed"+ message);
+//	@KafkaListener(topics = "kafka_demo_new",groupId = "kafka-grp1")
+//	public void consueme(String message) {
+//		log.info("message cosumed"+ message);
+//	}
+
+	@KafkaListener(topics = "kafka_demo_customer",groupId = "kafka-grp")
+	public void consueme(Customer customer) {
+		log.info("message cosumed"+ customer.toString());
 	}
 }
